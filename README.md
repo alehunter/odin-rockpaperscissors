8a0b668a77525ade6a2d599a5a27f659bc4344fb
alehunter <ahunt043@fiu.edu>
2022-09-09 14:29:45 -0400

    adding complete rps ui

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # odin-rockpaperscissors
+
+This is a simple UI for Rock Paper Scissors.
+
+When the page is loaded, the 'command' div is initialized with a default "Start Game" text.
+
+There are three option buttons on the left side. As soon as the user clicks on an option, a random choice from a choices array is assigned to computerChoice.
+
+The result is then compared to the user choice and the results are displayed in the 'command' div. \
+The computer choice is shown directly under.
--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
+
+const rock = document.getElementById('r');
+const paper = document.getElementById('p');
+const scissors = document.getElementById('s');
+const result = document.getElementById('result');
+const compresult = document.getElementById('computerchoice')
+
+
+result.textContent = 'Start Game!'
+
+
 const choices = ['rock', 'paper', 'scissors'];
 
 
@@ -5,77 +16,60 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-
-function getPlayerChoice(){
-    let input = prompt('Select a move: Rock, Paper or Scissors\n');
-    input = input.toLowerCase();
-    return input;
-
-}
-
-
-function playRound(){
-    playerSelection = getPlayerChoice();
-    computerSelection = getComputerChoice();
-    winner = ''
-    
+function getwinner(computerSelection, playerSelection){
+    let winner = ''
     if(computerSelection == choices[0] && playerSelection == choices[0]){
-        console.log('Draw! Nobody wins');
+        winner = 'nobody wins'
     }
     else if(computerSelection == choices[0] && playerSelection == choices[1]){
-        console.log('Player wins!');
-        winner = 'player';
+        winner = 'player wins';
     }
     else if(computerSelection == choices[0] && playerSelection == choices[2]){
-        console.log('Computer wins!');
-        winner = 'computer';
+        winner = 'computer wins';
     }
     else if(computerSelection == choices[1] && playerSelection == choices[0]){
-        console.log('Player wins!');
-        winner = 'player';
+        winner = 'player wins';
     }
     else if(computerSelection == choices[1] && playerSelection == choices[1]){
-        console.log('Draw! Nobody wins');
+        winner = 'nobody wins'
     }
     else if(computerSelection == choices[1] && playerSelection == choices[2]){
-        console.log('Player wins!');
-        winner = 'player';
+        winner = 'player wins';
     }
     else if(computerSelection == choices[2] && playerSelection == choices[0]){
-        console.log('Player wins!');
-        winner = 'player';
+        winner = 'player wins';
     }
     else if(computerSelection == choices[2] && playerSelection == choices[1]){
-        console.log('Computer wins!');
-        winner = 'computer';
+        winner = 'computer wins';
     }
     else if(computerSelection == choices[2] && playrerSelection == choices[2]){
-        console.log('Draw! Nobody wins');
+        winner = 'nobody wins'
     }
     return winner;
 }
 
-function playGame(){
-    computer_points = 0;
-    player_points = 0;
-    for (let i = 1; i <= 5; i++) {
-        console.log('Round: ' + i + '');
-        result = playRound();
-        if(winner === 'player'){
-            player_points++;
-        }else if(winner === 'computer'){
-            computer_points++;
-        }
-        
-    }
-    if(computer_points > player_points){
-        console.log('Computer wins!');
-    }else if(computer_points < player_points){
-        console.log('Player wins!');
-    }else{
-        console.log('Draw! Nobody wins');
-    }
-}
 
+rock.addEventListener('click', () => {
 
-playGame();
+    let compselection = getComputerChoice();
+    result.textContent = getwinner(compselection, 'rock');
+    compresult.textContent = "Computer chose: " + compselection;
+
+   
+});
+
+paper.addEventListener('click', () => {
+
+    let compselection = getComputerChoice();
+    result.textContent = getwinner(compselection, 'paper');
+    compresult.textContent = "Computer chose: " + compselection;
+
+});
+
+scissors.addEventListener('click', () => {
+
+    let compselection = getComputerChoice();
+    result.textContent = getwinner(compselection, 'scissors'); 
+    compresult.textContent = "Computer chose: " + compselection;
+
+});
